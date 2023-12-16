e49a4e497302416fc19c9eb20b39fa158bbd4d69
sandypockets <zac@znacreative.com>
2023-12-16 12:53:19 -0500

    Feat: Include cjs entry point

--- rollup.config.js
+++ rollup.config.js
@@ -3,20 +3,41 @@ import commonjs from '@rollup/plugin-commonjs';
 import postcss from 'rollup-plugin-postcss';
 import { terser } from 'rollup-plugin-terser';
 
-export default {
-  input: 'src/index.js',
-  output: {
-    file: 'dist/index.esm.js',
-    format: 'esm',
-    sourcemap: false,
+export default [
+  // ESM build
+  {
+    input: 'src/index.js',
+    output: {
+      file: 'dist/index.esm.js',
+      format: 'esm',
+      sourcemap: false,
+    },
+    plugins: [
+      resolve(),
+      commonjs(),
+      postcss({
+        extract: true,
+        minimize: true,
+      }),
+      terser(),
+    ],
   },
-  plugins: [
-    resolve(),
-    commonjs(),
-    postcss({
-      extract: true,
-      minimize: true,
-    }),
-    terser(),
-  ],
-};
+  // CommonJS build
+  {
+    input: 'src/index.js',
+    output: {
+      file: 'dist/index.js',
+      format: 'cjs',
+      sourcemap: false,
+    },
+    plugins: [
+      resolve(),
+      commonjs(),
+      postcss({
+        extract: true,
+        minimize: true,
+      }),
+      terser(),
+    ],
+  }
+];
