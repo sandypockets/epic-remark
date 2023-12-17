63b5df64d4796ef24e8b142bb322120777aada5c
sandypockets <zac@znacreative.com>
2023-12-16 21:21:39 -0500

    Fix: Export embed function

--- src/index-dark.js
+++ src/index-dark.js
@@ -3,5 +3,6 @@ import addHeadingIds from './plugins/addHeadingIds.js';
 import wrapElements from './plugins/wrapElements.js';
 import addTableOfContents from './plugins/addTableOfContents.js';
 import calculateReadingTime from './plugins/calculateReadingTime.js';
+import embed from './plugins/embed.js'
 import processMarkdown from './processMarkdown.js';
-export { addHeadingIds, wrapElements, addTableOfContents, calculateReadingTime, processMarkdown };
+export { addHeadingIds, wrapElements, addTableOfContents, calculateReadingTime, embed, processMarkdown };
